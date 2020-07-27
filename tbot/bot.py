a60797ad1b8e513bddde762158570706d86f6d90
Kostya Nikiforov <kostya.nik.3854@gmail.com>
2020-07-27 17:44:44 +0500

    fixed local setup

--- tbot/bot.py
+++ tbot/bot.py
@@ -1,3 +1,5 @@
+import asyncio
+
 from aiogram import types
 from aiogram.dispatcher import FSMContext
 from aiogram.dispatcher.filters.state import State, StatesGroup
@@ -11,12 +13,18 @@ from aiogram.contrib.fsm_storage.redis import RedisStorage
 
 from config import Config
 
+# For flask sessions
+try:
+    loop = asyncio.get_event_loop()
+except RuntimeError:
+    loop = asyncio.new_event_loop()
 
-bot = Bot(token=Config.TELEGRAM_TOKEN, parse_mode=ParseMode.HTML)
+bot = Bot(token=Config.TELEGRAM_TOKEN, parse_mode=ParseMode.HTML, loop=loop)
 storage = RedisStorage(
     host=Config.REDIS_HOST,
     port=Config.REDIS_PORT,
-    password=Config.REDIS_PASSWORD
+    password=Config.REDIS_PASSWORD,
+    loop=loop
 )
 dp = Dispatcher(bot, storage=storage)
 
