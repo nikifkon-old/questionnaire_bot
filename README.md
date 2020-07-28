a3abf32266d7bda785fb5c836f9c97e9b00ec02e
Kostya Nikiforov <kostya.nik.3854@gmail.com>
2020-07-28 17:44:02 +0500

    Moved requirements into setup.py, create dev-re...
    quirements. Also update project info

--- README.md
+++ README.md
@@ -3,31 +3,51 @@
 # Installation
 
 Create `.env` file
-```
+```bash
 cp .env.example .env
+```
+
+Install project
+```bash
+python setup.py develop
+```
+
+Install dev dependencies
+```bash
+pip install -r dev-requirements.txt
 ```
 
 ##### Note:
 
-Make sure your Postgres and Redis is working and the correct information is stored in the `.env` file
+> Make sure your Postgres and Redis is working and the correct information is stored in the `.env` file
 
 Run bot:
-```
+```bash
 python tbot/bot.py
 ```
 
 Run flask dev server
-```
+```bash
 flask run
 ```
 
 ## Migrations
 Create new migrations
-```
+```bash
 alembic revision --autogenerate -m "Message"
 ```
 
 Run existing migrations
-```
+```bash
 alembic upgrade head
+```
+
+## Tests
+```bash
+pytest
+```
+
+## Code style
+```bash
+flake8
 ```
--- setup.py
+++ setup.py
@@ -1,7 +1,33 @@
-from setuptools import find_packages, setuptools
+from setuptools import find_packages, setup
 
-setuptools.setup(
+install_requires = [
+    "aiogram",
+    "aioredis",
+    "psycopg2",
+    "Flask",
+    "gunicorn",
+    "Flask-Admin",
+    "Flask-Login",
+    "SQLAlchemy",
+    "alembic",
+    "pydantic",
+    "python-dotenv"
+]
+
+
+setup(
     name="tbot",
-    version="0.1",
-    packages=find_packages()
+    version="0.0.2",
+    description="",
+    keywords=[],
+    url="https://github.com/nikifkon/questionnaire_bot.git",
+    classifiers=[],
+    author="Konstantin Nikiforov",
+    author_email="kosty.nik.3854@gmail.com",
+    license="MIT",
+    packages=find_packages(),
+    setup_requires=[],
+    install_requires=install_requires,
+    include_package_data=True,
+    zip_safe=False,
 )
